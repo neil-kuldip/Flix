d2f009a4597f41a0ddde694fee37ca4726b7c7ae
Neil Kuldip <56613674+neil-kuldip@users.noreply.github.com>
2021-02-23 02:31:54 -0500

    Implemented DetailActivity.java, activity_detail.xml, used glide implementation to round poster images, and implemented a YouTubePlayer to view movie trailers.

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,24 +1,36 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 
+import java.lang.annotation.Target;
 import java.util.List;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
+
+import org.parceler.Parcels;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
@@ -58,12 +70,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+       RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(View itemView) {
             super(itemView);
+            container = itemView.findViewById(R.id.container);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
@@ -83,7 +97,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             else {
                 imageURL = movie.getPosterPath();
             }
-            Glide.with(context).load(imageURL).into(ivPoster);
+            int radius = 30;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            {
+                Glide.with(context)
+                        .load(imageURL)
+                        .transform(new RoundedCorners(radius))
+                        .override(1000, 400)
+                        .into(ivPoster);
+            }
+            else {
+                Glide.with(context)
+                        .load(imageURL)
+                        .transform(new RoundedCorners(radius))
+                        .override(350, 300)
+                        .into(ivPoster);
+            }
+            // 1} Register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 2) Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,22 +3,33 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+    String releaseDate;
+
+    // empty constructor needed by the Parceler library
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         backdropPath = jsonObject.getString("backdrop_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        releaseDate = jsonObject.getString("release_date");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -46,4 +57,13 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
 
+    public double getRating() { return rating; }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
 }
